08788a2079e9ed4afb2e694a24dcb722f7f31c7f
Caleb Lee <caleb.gwl@gmail.com>
2017-02-16 17:15:43 -0600

    Create and import navbar.scss and reset.scss

--- app/exercise/add-message-item.js
+++ app/exercise/add-message-item.js
@@ -1,1 +1,6 @@
-export default null;
+import createMessageItem from './create-message-item.js';
+
+export default function addMessageItem(list, object) {
+  const listEl = createMessageItem(object);
+  list.appendChild(listEl);
+}
